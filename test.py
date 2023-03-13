71208e1bc0a5d6d2dd840d22c593604ae18afe44
Ahmed Gabr <ahmedoy34@gmail.com>
2023-03-13 11:14:18 +0200

    Added Avg of Multiple Runs in Test

--- test.py
+++ test.py
@@ -2,50 +2,60 @@ import numpy as np
 import timeit
 
 
+gen_arr = """
+np.random.seed(7)
+arr = np.random.randint(0, 10000, size=(300,))
+"""
+
+
 setup1 = """
 import numpy as np
 import quick_sort_v1
-np.random.seed(7)
-arr1 = np.random.randint(0, 10000, size=(50000,))
 """
 
 setup2 = """
 import numpy as np
 import quick_sort_v2
-np.random.seed(7)
-arr2 = np.random.randint(0, 10000, size=(50000,))
 """
 
 
 setup3 = """
 import numpy as np
 from Sorting import mergeSort
-np.random.seed(7)
-arr3 = np.random.randint(0, 10000, size=(50000,))
 """
 
 
 setup4 = """
 import numpy as np
 import quick_sort_v3
-np.random.seed(7)
-arr4 = np.random.randint(0, 10000, size=(50000,))
-
 """
 
-test1 = "quick_sort_v1.quick_sort(arr1, 0, len(arr1)-1)"
+test1 = f"""
+{gen_arr}
+quick_sort_v1.quick_sort(arr, 0, len(arr)-1)
+"""
 
+test2 =f"""
+{gen_arr}
+quick_sort_v2.quick_sort(arr, 0, len(arr)-1)
+"""
 
-test2 = "quick_sort_v2.quick_sort(arr2, 0, len(arr2)-1)"
+test3 =f"""
+{gen_arr}
+mergeSort(arr, 0, len(arr)-1)
+"""
 
-test3 = "mergeSort(arr3, 0, len(arr3)-1)"
+test4 = f"""
+{gen_arr}
+quick_sort_v3.quicksort(arr, 0, len(arr)-1)
+"""
 
-test4 = "quick_sort_v3.quicksort(arr4, 0, len(arr4)-1)"
+number = 1000
+extra = timeit.timeit(stmt= gen_arr,setup=setup1, number=number)
+v1 = timeit.timeit(stmt=test1, setup=setup1, number=number)
+v2 = timeit.timeit(stmt=test2, setup=setup2, number=number)
+v4 = timeit.timeit(stmt=test4, setup=setup4, number=number)
 
-
-
-
-print("Quick v1",timeit.timeit(stmt=test1, setup=setup1, number=1))
-print("Quick v2",timeit.timeit(stmt=test2, setup=setup2, number=1))
-print("Merge",timeit.timeit(stmt=test3, setup=setup3, number=1))
-print("Quick v4",timeit.timeit(stmt=test4, setup=setup4, number=1))
+print((v1-extra)/number)
+print((v2-extra)/number)
+print((v4-extra)/number)
